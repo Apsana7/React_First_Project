8541ca3b6a08fd5f2609af67b0eac56768ead6af
Apsana7 <apsananeupane124@gmail.com>
2024-12-13 13:17:06 +0545

    state

--- src/Components/Navigation/Header.jsx
+++ src/Components/Navigation/Header.jsx
@@ -7,35 +7,35 @@ const Header = () => {
     return (
         <div>
             <div className='w-full'>
-                <div className='flex gap-24 px-28 border h-10 items-center justify-between '>
+                <div className='flex gap-24 px-24 border h-10 items-center justify-between '>
                     <div className='flex flex-row gap-6 text-sm'>
                         <div className='mx-15'>About Us</div>
                         <div>Privacy</div>
                         <div>FAQ</div>
                         <div>Careers</div>
                     </div>
-                    <div className='flex gap-4'>
-                        <div className='flex flex-row gap-4 text-gray-600 text-sm '>
+                    <div className='flex gap-12'>
+                        <div className='flex flex-row gap-4 text-gray-600 text-sm'>
                             <div>My Wishlist</div>
-                            <div>Track Your Order</div>
+                            <div className='border-x  px-6 border-gray-300'>Track Your Order</div>
                         </div>
-                        <div className='flex gap-4'>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95" /></svg></div>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3" /></svg></div>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.2 4.2 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.52 8.52 0 0 1-5.33 1.84q-.51 0-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23" /></svg></div>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512"><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3M447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2c-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3c94 0 111.28 61.9 111.28 142.3V448z" /></svg></div>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73" /></svg></div>
+                        <div className='flex gap-4 items-center text-gray-500'>
+                            <div className=' hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95" /></svg></div>
+                            <div className=' hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3" /></svg></div>
+                            <div className=' hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.2 4.2 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.52 8.52 0 0 1-5.33 1.84q-.51 0-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23" /></svg></div>
+                            <div className=' hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512"><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3M447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2c-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3c94 0 111.28 61.9 111.28 142.3V448z" /></svg></div>
+                            <div className=' hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73" /></svg></div>
                         </div>
                     </div>
                 </div>
 
 
-                <div className='bg-transparent flex gap-10 justify-between items-center px-24 py-6' >
+                <div className='bg-transparent flex gap-10 justify-between items-center px-24 py-4' >
                     <div className=' flex gap-3 items-center'>
                    <div className='text-white text-5xl bg-red-500 rounded-full'> <IoHomeOutline /> </div>
                         <span className='font-bold text-blue-950 text-xl'>Capital</span><h1 className='text-xl'>Shop</h1></div>
 
-                    <div className=' flex gap-10  font-semibold'>
+                    <div className=' flex gap-14  font-semibold'>
                         <Link to='/ ' className=' hover:text-red-500'>Home</Link>
                         <Link to='/Men' className=' hover:text-red-500'>Men</Link>
                         <Link to='/Women' className=' hover:text-red-500'>Women</Link>
@@ -46,10 +46,10 @@ const Header = () => {
                     </div>
 
 
-                    <div className="flex  gap-10 px-10  ">
-                        <div><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989t-1.96.35q-2.402 0-4.066-1.663T3.808 9.503T5.47 5.436t4.064-1.667t4.068 1.664T15.268 9.5q0 1.042-.369 2.017t-.97 1.668l6.262 6.261zM9.539 14.23q1.99 0 3.36-1.37t1.37-3.361t-1.37-3.36t-3.36-1.37t-3.361 1.37t-1.37 3.36t1.37 3.36t3.36 1.37" /></svg></div>
-                        <div><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 20v-1a7 7 0 0 1 7-7v0a7 7 0 0 1 7 7v1m-7-8a4 4 0 1 0 0-8a4 4 0 0 0 0 8" /></svg></div>
-                        <div><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 6h19l-3 10H6zm0 0l-.75-2.5M9.992 11h2m2 0h-2m0 0V9m0 2v2M11 19.5a1.5 1.5 0 0 1-3 0m9 0a1.5 1.5 0 0 1-3 0" /></svg></div>
+                    <div className="flex gap-10 ">
+                        <div className='hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989t-1.96.35q-2.402 0-4.066-1.663T3.808 9.503T5.47 5.436t4.064-1.667t4.068 1.664T15.268 9.5q0 1.042-.369 2.017t-.97 1.668l6.262 6.261zM9.539 14.23q1.99 0 3.36-1.37t1.37-3.361t-1.37-3.36t-3.36-1.37t-3.361 1.37t-1.37 3.36t1.37 3.36t3.36 1.37" /></svg></div>
+                        <div className='hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 20v-1a7 7 0 0 1 7-7v0a7 7 0 0 1 7 7v1m-7-8a4 4 0 1 0 0-8a4 4 0 0 0 0 8" /></svg></div>
+                        <div className='hover:text-red-500'><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 6h19l-3 10H6zm0 0l-.75-2.5M9.992 11h2m2 0h-2m0 0V9m0 2v2M11 19.5a1.5 1.5 0 0 1-3 0m9 0a1.5 1.5 0 0 1-3 0" /></svg></div>
                     </div>
                 </div>
 
--- src/Components/Page Component/Home/Banner.jsx
+++ src/Components/Page Component/Home/Banner.jsx
@@ -1,15 +1,93 @@
-import React from 'react'
-import { IoIosArrowDropleft } from "react-icons/io";
-import { FiArrowRightCircle } from "react-icons/fi";
+import React, { useEffect, useLayoutEffect } from 'react'
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Banner = () => {
+    const images = [
+        'https://i.pinimg.com/736x/3a/b6/f0/3ab6f061cbc449ee858188a9ff3f4b25.jpg',
+        'https://i.pinimg.com/736x/76/e6/cb/76e6cb3049c9bc5fbf4d691e37660b38.jpg'
+        
+    ];
+
+    const [slide, setSlide] = useState(0)
+    function nextInd() {
+        if (images.length - 1 == slide) {
+            setSlide(0)
+        } else {
+            setSlide(slide + 1)
+        }
+    }
+    function prevInd() {
+        if (slide == 0) {
+            setSlide(images.length - 1)
+        } else {
+            setSlide(slide - 1)
+        }
+    }
+
+    //    useEffect(() => {
+    //       let interval;
+
+    //       interval = setTimeout(()=>{
+    //         nextInd()
+    //       },2000);
+
+    //      return () => {
+    //        clearTimeout(interval)
+    //      }
+    //    }, [slide])
+
+
     return (
-        <div className='flex flex-row relative'>
-            <div className='text-6xl absolute top-0'><IoIosArrowDropleft /></div>
-            <div> <img src="https://www.holland.com/upload_mm/2/4/4/80160_fullimage_rondvaartboot%20vaart%20onder%20brug%20door%20met%20mooie%20wolkenlucht%20%C2%A9%20illusion-x%20via%20pixabay_1150x663_438x353.jpg" alt="" className='h-100  w-screen' /></div>
-            <div className='text-6xl absolute right-0'><FiArrowRightCircle /></div>
-        </div>
+        <>
+            <div>
+
+                <div className='absolute flex flex-row overflow-clip  '>
+                    <div onClick={() => { prevInd() }} className='text-6xl absolute  top-1/2 z-10  bg-gray-200 text-white  py-5  w-fit h-fit cursor-pointer' ><MdOutlineKeyboardArrowLeft /></div>
+
+                    <Carousel  autoPlay interval={1000} showStatus={false} infiniteLoop className='w-full' showThumbs={false} renderIndicator={false} showArrows={false}>
+                        {
+                            images.map((val, i) => {
+                                return (
+                                    <img src={val}  className='w-full'/>
+                                    // <img src={val} alt="" className={`h-100 w-screen absolute ${i == slide  ? "translate-x-0" : i < slide ?
+                                    //     "translate-x-[10000px]" : "-translate-x-[10000px]"} transition-all delay-75 duration-1000  top-0 }`}/>
+                                )
+                            })
+                        }
+                    </Carousel  >
+
+                    <div onClick={()=>{nextInd()}} className='text-6xl absolute top-1/2 right-0  bg-gray-200 text-white p-2  cursor-pointer' ><MdOutlineKeyboardArrowRight /></div>
+                </div>
+
+
+            <div className='relative text-white'>
+                <div>Fashion Sale</div>
+                <div>Minimal Menz Style</div>
+                <div>Cohgk alhfajkhf afhjkdsfhkalhsf hfajfhahfujskljk</div>
+                <button>Shop Now</button>
+            </div>
+
+            </div>
+        </>
     )
 }
-
 export default Banner
+
+
+
+
+
+
+
+
+
+
+
+
+
+
